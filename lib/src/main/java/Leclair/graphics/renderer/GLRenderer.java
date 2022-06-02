e0edac4003601ce28de58feabd602476113f77ad
Licone7 <brettb007@outlook.com>
2022-06-02 14:13:19 -0400

    Undo most work done for OpenGL since it needs a re-write

--- lib/src/main/java/Leclair/graphics/renderer/GLRenderer.java
+++ lib/src/main/java/Leclair/graphics/renderer/GLRenderer.java
@@ -2,25 +2,11 @@ package Leclair.graphics.renderer;
 
 import static org.lwjgl.opengl.GL33C.*;
 
-import java.nio.ByteBuffer;
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.util.ArrayList;
-import java.util.List;
-
-import Leclair.asset.AssetLoader;
 import Leclair.graphics.scene.Mesh;
-import Leclair.graphics.scene.RenderStates;
 import Leclair.graphics.scene.ViewPort;
 import Leclair.graphics.shader.Shader;
-import Leclair.graphics.shader.Shaders;
 import Leclair.math.Color;
-import Leclair.math.Vector3;
 import Leclair.window.WindowInfo;
-import org.joml.Matrix4f;
-import org.joml.Quaternionf;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.PointerBuffer;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
 import org.lwjgl.opengl.WGL;
@@ -42,22 +28,6 @@ public class GLRenderer implements GraphicsRenderer {
 
     static ViewPort viewPort;
     static GLCapabilities capabilities;
-
-    static List<Integer> vaos = new ArrayList<>();
-    static List<Integer> programs = new ArrayList<>();
-    static List<Integer> vertShaders = new ArrayList<>();
-    static List<Integer> fragShaders = new ArrayList<>();
-    static List<Integer> textures = new ArrayList<>();
-    static int ubo;
-    static int transWell;
-    static int matId;
-
-    Matrix4f projectionMatrix = new Matrix4f();
-    Quaternionf orientation = new Quaternionf();
-    private final Vector3 position = new Vector3(0, 2, 5);
-    boolean viewing;
-    float mouseX;
-    float mouseY;
 
     public GLRenderer(final ViewPort vp) {
         viewPort = vp;
@@ -92,7 +62,6 @@ public class GLRenderer implements GraphicsRenderer {
                     // TODO
                     break;
             }
-            position.negate();
             capabilities = GL.createCapabilities(true);
             glEnable(GL_DEPTH_TEST);
             glEnable(GL_STENCIL_TEST);
@@ -116,78 +85,18 @@ public class GLRenderer implements GraphicsRenderer {
 
     @Override
     public void loop() {
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            FloatBuffer UtilityFB2 = stack.mallocFloat(16); // TODO: We don't need this
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            for (final Mesh mesh : Mesh.getMeshes()) {
-                if (mesh.getState() == RenderStates.STATE_RENDER) {
-                    glUseProgram(programs.get(mesh.index));
-                    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo);
-                    int uniformId = glGetUniformBlockIndex(programs.get(mesh.index), "camera");
-                    glUniformBlockBinding(programs.get(mesh.index), uniformId, ubo);
-                    FloatBuffer u = viewPort.getCamera().getProjectionMatrix().get(UtilityFB2);
-                    glBufferData(GL_UNIFORM_BUFFER, u, GL_DYNAMIC_DRAW);
-                    glBufferSubData(GL_UNIFORM_BUFFER, 64, u);
-                    glUnmapBuffer(GL_UNIFORM_BUFFER);
-                    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-                    glBindBufferBase(GL_UNIFORM_BUFFER, 1, transWell);
-                    int uniformId2 = glGetUniformBlockIndex(programs.get(mesh.index), "scene");
-                    glUniformBlockBinding(programs.get(mesh.index), uniformId2, transWell);
-                    FloatBuffer u2 = mesh.transMat.get(UtilityFB2);
-                    glBufferData(GL_UNIFORM_BUFFER, u2, GL_DYNAMIC_DRAW);
-                    glBufferSubData(GL_UNIFORM_BUFFER, 64, u2);
-                    glUnmapBuffer(GL_UNIFORM_BUFFER);
-                    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-
-
-
-
-
-
-
-                    glBindVertexArray(vaos.get(mesh.index));
-                    glEnableVertexAttribArray(0);
-                    glBindTexture(GL_TEXTURE_2D, textures.get(mesh.index));
-                    glDrawArrays(GL_TRIANGLES, 0, mesh.getVertexNumber());
-                    glBindTexture(GL_TEXTURE_2D, 0);
-                    glDisableVertexAttribArray(0);
-                    glBindVertexArray(0);
-
-                    glBindBufferBase(GL_UNIFORM_BUFFER, 2, matId);
-                    int uniformId3 = glGetUniformBlockIndex(programs.get(mesh.index), "material");
-                    glUniformBlockBinding(programs.get(mesh.index), uniformId3, matId);
-                    FloatBuffer u3 = stack.mallocFloat(14);
-                    u3.put(0, mesh.material.getAmbientColor().getR());
-                    u3.put(1, mesh.material.getAmbientColor().getG());
-                    u3.put(2, mesh.material.getAmbientColor().getB());
-                    u3.put(3, mesh.material.getAmbientColor().getA());
-                    u3.put(4, mesh.material.getDiffuseColor().getR());
-                    u3.put(5, mesh.material.getDiffuseColor().getG());
-                    u3.put(6, mesh.material.getDiffuseColor().getB());
-                    u3.put(7, mesh.material.getDiffuseColor().getA());
-                    u3.put(8, mesh.material.getSpecularColor().getR());
-                    u3.put(9, mesh.material.getSpecularColor().getG());
-                    u3.put(10, mesh.material.getSpecularColor().getB());
-                    u3.put(11, mesh.material.getSpecularColor().getA());
-                    u3.put(12, 1f);
-                    u3.put(13, 0f);// reflect
-                    glBufferData(GL_UNIFORM_BUFFER, u3, GL_DYNAMIC_DRAW);
-                    glBufferSubData(GL_UNIFORM_BUFFER, 0, u3);
-                    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-                }
-            }
-            glViewport(0, 0, WindowInfo.getWidth(), WindowInfo.getHeight());
-            switch (Platform.get()) {
-                case WINDOWS:
-                    GDI32.SwapBuffers(hdc);
-                    break;
-                case MACOSX:
-                    // TODO
-                    break;
-                case LINUX:
-                    // TODO
-                    break;
-            }
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, WindowInfo.getWidth(), WindowInfo.getHeight());
+        switch (Platform.get()) {
+            case WINDOWS:
+                GDI32.SwapBuffers(hdc);
+                break;
+            case MACOSX:
+                // TODO
+                break;
+            case LINUX:
+                // TODO
+                break;
         }
     }
 
@@ -198,74 +107,12 @@ public class GLRenderer implements GraphicsRenderer {
 
     @Override
     public void processMesh(final Mesh mesh) {
-        final int id = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, id);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mesh.material.getTexture().getWidth(),
-                mesh.material.getTexture().getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                mesh.material.getTexture().getData());
-        glBindTexture(GL_TEXTURE_2D, 0);
-        textures.add(mesh.index, id);
 
-        final int program = glCreateProgram();
-        addShader(mesh.material.getVertexShader(), program, Mesh.meshes.indexOf(mesh));
-        addShader(mesh.material.getFragmentShader(), program, Mesh.meshes.indexOf(mesh));
-        programs.add(mesh.index, program);
-
-        glLinkProgram(programs.get(mesh.index));
-        final int linked = glGetProgrami(programs.get(mesh.index), GL_LINK_STATUS);
-        final String programLog = glGetProgramInfoLog(programs.get(mesh.index));
-        if (programLog.trim().length() > 0)
-            System.err.println(programLog);
-        if (linked == 0)
-            throw new AssertionError("Could not link program");
-        glUseProgram(programs.get(mesh.index));
-
-        final int vao = glGenVertexArrays();
-        vaos.add(mesh.index, vao);
     }
 
     @Override
     public void renderMesh(final Mesh mesh) {
-        final int texLocation = glGetUniformLocation(programs.get(mesh.index), "tex");
-        glUniform1i(texLocation, 0);
-        final int inputPosition = glGetAttribLocation(programs.get(mesh.index), "position");
-        final int inputTextureCoords = glGetAttribLocation(programs.get(mesh.index), "texCoords");
-        int uboId = glGenBuffers();
-        ubo = uboId;
-
-        int tra = glGenBuffers();
-        transWell = tra;
-
-        int mat = glGenBuffers();
-        matId = mat;
-
-        glUseProgram(0);
-
-        final int vao = vaos.get(mesh.index);
-        glBindVertexArray(vao);
-        final int positionVbo = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, positionVbo);
-        glBufferData(GL_ARRAY_BUFFER, mesh.getData(), GL_STATIC_DRAW);
-        glVertexAttribPointer(inputPosition, 4, GL_FLOAT, false, 0, 0L);
-        glEnableVertexAttribArray(inputPosition);
-
-        final int texCoordsVbo = glGenBuffers();
-        final FloatBuffer fb = BufferUtils.createFloatBuffer(2 * 6);
-        fb.put(0.0f).put(1.0f);
-        fb.put(1.0f).put(1.0f);
-        fb.put(1.0f).put(0.0f);
-        fb.put(1.0f).put(0.0f);
-        fb.put(0.0f).put(0.0f);
-        fb.put(0.0f).put(1.0f);
-        fb.flip();
-        glBindBuffer(GL_ARRAY_BUFFER, texCoordsVbo);
-        glBufferData(GL_ARRAY_BUFFER, fb, GL_STATIC_DRAW);
-        glVertexAttribPointer(inputTextureCoords, 2, GL_FLOAT, true, 0, 0L);
-        glEnableVertexAttribArray(inputTextureCoords);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
+        
     }
 
     @Override
@@ -275,7 +122,7 @@ public class GLRenderer implements GraphicsRenderer {
 
     @Override
     public void deleteMesh(final Mesh mesh) {
-        // glDeleteVertexArrays();
+
     }
 
     @Override
@@ -285,39 +132,7 @@ public class GLRenderer implements GraphicsRenderer {
 
     @Override
     public void addShader(final Shader shader, final int program, final int index) {
-        final PointerBuffer strings = BufferUtils.createPointerBuffer(1);
-        final IntBuffer lengths = BufferUtils.createIntBuffer(1);
-        switch (shader.shaderType) {
-            case Shaders.VERTEX_SHADER:
-                final int vshader = glCreateShader(GL_VERTEX_SHADER);
-                ByteBuffer source = null;
-                source = AssetLoader.importAsBinary(shader.filePath, 8192);
-                strings.put(0, source);
-                lengths.put(0, source.remaining());
-                glShaderSource(vshader, strings, lengths);
-                glCompileShader(vshader);
-                glAttachShader(program, vshader);
-                vertShaders.add(index, vshader);
-                strings.clear();
-                lengths.clear();
-                break;
-            case Shaders.FRAGMENT_SHADER:
-                final int fshader = glCreateShader(GL_FRAGMENT_SHADER);
-                source = AssetLoader.importAsBinary(shader.filePath, 8192);
-                strings.put(0, source);
-                lengths.put(0, source.remaining());
-                glShaderSource(fshader, strings, lengths);
-                glCompileShader(fshader);
-                glAttachShader(program, fshader);
-                fragShaders.add(index, fshader);
-                strings.clear();
-                lengths.clear();
-                break;
-            case Shaders.COMPUTE_SHADER:
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid shader!");
-        }
+
     }
 
     @Override
